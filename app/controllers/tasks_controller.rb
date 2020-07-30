6a4e764d916ca25307c4bf6e36e1ce1af15e8c1d
morpho182 <morpho182.b@gmail.com>
2020-07-30 06:00:13 +0000

    correct_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,12 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :show, :edit, :update]
   
   def index
-    if logged_in?
-      # @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -19,19 +15,16 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.build(task_params)
-
     if @task.save
       flash[:success] = 'タスクが追加されました。'
       redirect_to root_url
     else
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクの追加に失敗しました。'
-      render 'tasks/index'
+      render new 
     end
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
